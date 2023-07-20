4df028d8f4843a05f45f35b1dca08ae4727d1625
AxlMi <47877419+AxlMi@users.noreply.github.com>
2023-07-20 21:36:40 +0200

    Update join.ps1

--- join.ps1
+++ join.ps1
@@ -5,20 +5,19 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$AdminUsername,
     [Parameter(Mandatory=$true)]
-    [SecureString]$AdminPassword,
+    [string]$AdminPassword,
     [Parameter(Mandatory=$true)]
     [string]$DNSIPAddress
 )
 
-# Convert secure string password to plain text
-$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AdminPassword)
-$PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+# Convert password to SecureString
+$SecurePassword = ConvertTo-SecureString -String $AdminPassword -AsPlainText -Force
 
 # Create a credential object
-$Credential = New-Object System.Management.Automation.PSCredential ($AdminUsername, $AdminPassword)
+$Credential = New-Object System.Management.Automation.PSCredential ($AdminUsername, $SecurePassword)
 
-# Set DNS server address
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $DNSIPAddress
+# Change the DNS settings
+Set-DNSClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $DNSIPAddress
 
 # Join the domain
 Add-Computer -DomainName $DomainName -Credential $Credential -Restart -Force
